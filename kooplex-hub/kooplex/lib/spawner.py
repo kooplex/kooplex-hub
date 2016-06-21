47ab43dfc1c684235bf0f9cd2570cfc2bf3ef9ce
Laszlo Dobos <dobos@complex.elte.hu>
2016-06-21 17:43:28 +0200

    Kernel spawning works, needs logic to configure container IP address.

--- kooplex-hub/kooplex/lib/spawner.py
+++ kooplex-hub/kooplex/lib/spawner.py
@@ -1,12 +1,13 @@
-import sys
+﻿import sys
 import getopt
 import json
-import socket
 import string
+import uuid
 import random
 import docker
 import requests as req
 
+from netaddr import IPAddress
 from time import sleep
 from io import BytesIO
 from kooplex.lib.libbase import LibBase
@@ -19,18 +20,20 @@ class Spawner(LibBase):
         self.docker_port = get_settings('KOOPLEX_SPAWNER', 'docker_port', port)
         self.docker_network = get_settings('KOOPLEX_SPAWNER', 'docker_network', 'bridge')
         self.docker_container_prefix = get_settings('KOOPLEX_SPAWNER', 'docker_container_prefix', 'kooplex-jupyter')
-        self.docker_image = get_settings('KOOPLEX_SPAWNER', 'docker_image', (image, 'kooplex-jupyter'))
+        self.docker_image = get_settings('KOOPLEX_SPAWNER', 'docker_image', (image, 'jupyter/minimal-notebook')) # TODO: replace with our image
 
         self.proxy_host = get_settings('KOOPLEX_SPAWNER', 'proxy_host', '127.0.0.1')
         self.proxy_port = get_settings('KOOPLEX_SPAWNER', 'proxy_port', 8000)
-        self.proxy_path = get_settings('KOOPLEX_SPAWNER', 'proxy_path', 'hub/%s')
+        self.proxy_path = get_settings('KOOPLEX_SPAWNER', 'proxy_path', 'hub/%s/%s')
         self.proxy_api_port = get_settings('KOOPLEX_SPAWNER', 'proxy_api_port', 8001)
+        self.proxy_external_url = get_settings('KOOPLEX_SPAWNER', 'proxy_external_url', 'http://localhost/')
         
+        self.spawner_ip_pool = get_settings('KOOPLEX_SPAWNER', 'spawner_ip_pool', ['172.17.20.1', '172.17.20.255'])
         self.spawner_port_pool = get_settings('KOOPLEX_SPAWNER', 'spawner_port_pool', [16000, 17000])
         
         self.username = username
         
-        self.docli = self.create_docker_client()
+        self.docli = self.create_docker_client()        
 
     ### -------------------------------------------------------
     ### Docker setup
@@ -42,67 +45,114 @@ class Spawner(LibBase):
             url = 'tcp://%s:%d' % (self.docker_host, self.docker_port)
         return docker.Client(base_url=url)
 
-    def get_container_name():
-        return '%s-%s' % (Spawner.docker_container_prefix, self.username)
+    def get_network(self):
+        nets = self.docli.networks(names = (self.docker_network,))
+        if nets and len(nets) == 1:
+            return nets[0]
+        else:
+            return None
+
+    def get_image(self):
+        imgs = self.docli.images(name=self.docker_image)
+        if imgs and len(imgs) == 1:
+            return imgs[0]
+        else:
+            return None
+
+    def pull_image(self):
+        self.docli.pull(self.docker_image)
+        return self.get_image()
+
+    def build_image(self):
+        raise NotImplementedError
 
     def ensure_image_exists(self):
-        imgs = docli.images(name=self.docker_image)
-        if imgs is None or len(imgs) == 0:
-            docli.pull(self.docker_image)
-        return docli.images(name=self.docker_image)
+        img = self.get_image()
+        if img is None:
+            img = self.pull_image()
+        return img
+
+    def get_container(self, name):
+        conts = self.docli.containers(all=True,
+            filters={
+            'name': name,
+            })
+        if conts and len(conts) == 1:
+            return conts[0]
+        else:
+            return None
 
     def list_containers(self):
         containers = docli.containers(all=True)
         # TODO: modify to return user's containers only
-        #containers = [ c for c in docli.containers() if Spawner.container_prefix in c['Names'][0] ]
+        #containers = [ c for c in docli.containers() if
+        #Spawner.container_prefix in c['Names'][0] ]
         return containers
 
-    def create_container(self):
+    def create_container(self, name):
         self.ensure_image_exists()
-        name = self.get_container_name()
-        container = docli.create_container(
-            image=self.docker_image,
+        container = self.docli.create_container(image=self.docker_image,
             detach=True,
             name=name,
-            command=command,
+            #command=command,
             #entrypoint='start-notebook.sh --NotebookApp.base_url=/' + name
         )
-        docli.disconnect_container_from_network(name, 'bridge')
-        docli.connect_container_to_network(name, self.docker_network)
+        self.docli.disconnect_container_from_network(name, 'bridge')
+        self.docli.connect_container_to_network(name, self.docker_network)
+        return self.get_container(name)
+
+    def ensure_container_exists(self, name):
+        cont = self.get_container(name)
+        if cont is None:
+            cont = self.create_container(name)
+        return cont
+
+    def start_container(self, name):
+        container = self.ensure_container_exists(name)
+        self.docli.start(container)
+        return self.get_container(name)
+
+    def ensure_container_running(self, name):
+        container = self.ensure_container_exists(name)
+        # created|restarting|running|paused|exited|dead
+        if container['State'] in ('created', 'exited') :
+            container = self.start_container(name)
         return container
 
-    def ensure_container_exists(self):
-        name = self.get_container_name()
-        conts = docli.containers(filters={'name': name})
-        if conts is None or len(conts) == 0:
-            self.create_container()
-        return docli.containers(filters={'name': name})[0]
+    def stop_container(self, name):
+        self.docli.stop(container=name)
 
-    def start_container(self):
-        container = self.ensure_container_exists()
-        docli.start(container)
-        return container
+    def kill_container(self, name):
+        self.docli.kill(container=name)
 
-    def ensure_container_running(self, name, command):
-        name = self.get_container_name()
-        container = self.ensure_container_exists()
-        if container['status'] == 'Stopped':
-            self.start_container()
-        return docli.containers(filters={'name': name})[0]
+    def ensure_container_stopped(self, name):
+        container = self.get_container(name)
+        if container:
+            self.stop_container(name)
+
+    def remove_container(self, name):
+        self.docli.remove_container(name)
+
+    def ensure_container_removed(self, name):
+        container = self.get_container(name)
+        if container:
+            self.ensure_container_stopped(name)
+            self.remove_container(name)
+
+    def exec_container(self, name, command):
+        exec = self.docli.exec_create(container=name, cmd=command, user=self.username)
+        self.docli.exec_start(exec)
 
     ### -------------------------------------------------------
     ### Proxy setup
 
     def create_route(self, path, host=None, port=None):
-        path = self.proxy_path % path
-        url = 'http://%s:%d/api/routes/%s' % (
-            self.proxy_host,
+        url = 'http://%s:%d/api/routes/%s' % (self.proxy_host,
             self.proxy_api_port,
             path)
         if host:
             data = json.dumps({
-                'target': 'http://%s:%d/%s' % (
-                    host, 
+                'target': 'http://%s:%d/%s' % (host, 
                     port,
                     path)
                 })
@@ -123,46 +173,57 @@ class Spawner(LibBase):
         res = self.http_delete(url)
 
     def get_external_url(self, path, port):
-        url = 'http://%s/%s' % (
-            self.proxy_external_host,
-            path)
+        url = 'http://%s/%s' % (self.proxy_external_host,  path)
         return url
 
     ### -------------------------------------------------------
     ### Kernel spawner
 
     def get_random_port(self):
+        # TODO: modify to find available port
         return random.randint(self.spawner_port_pool[0], self.spawner_port_pool[1])
 
-    def get_kernel_command(self):
-        return 'start-notebook.sh --NotebookApp.base_url=/' + self.username
+    def get_kernel_container(self):
+        return '%s-%s' % (self.docker_container_prefix, self.username)
 
-    def spawn_kernel(self, container_name, port):
-        container = self.ensure_container_running(name)
-        exec = docli.exec_create(
-            name=container_name,
-            cmd=self.get_kernel_command(),
-            user=self.username)
-        docli.exec_start(exec)
-        add_proxy_rule()
+    def get_kernel_path(self, id):
+        return self.proxy_path % (self.username, id)
 
+    def get_kernel_command(self, path, port):
+        command = 'start-notebook.sh --NotebookApp.base_url=/%s --NotebookApp.port=%d' % (path, port)
+        return command
 
-    def spawn_container(self, container_name, command, port):
-        """
-        Simple function to spawn a container from an image.
-        The container is connected to the same docker network
-        as "poor man's binder" is.
-        It is assumed that start-notebook.sh exists and
-        can be run in the image as it will be our entry point.
-        """
+    def get_external_url(self, path):
+        url = self.proxy_external_url + path
+        return url
 
-        # starting the container and connecting it to the
-        # appropriate docker network
-        docli.start(container)
+    def get_kernel_info(self, username, id, host, container, port, path, url):
+        data = json.dumps({
+            'id': id,
+            'host': host,
+            'container': container,
+            'port': port,
+            'path': path,
+            'url': url
+        })
+        return data
+
+    def spawn_kernel(self):
+        id = str(uuid.uuid4())
+        host = self.docker_host
+        port = self.get_random_port()
+        container = self.get_kernel_container()
+        path = self.get_kernel_path(id)
+        command = self.get_kernel_command(path, port)
+        url = self.get_external_url(path)
         
+        self.ensure_container_running(container)
+        self.exec_container(container, command)
+        
+        self.remove_route(path)
+        self.add_route(path, host, port)
 
-        #proxy = Proxy()
-        #rule = proxy.add_rule(container_name, port)
-        rule = None
+        return self.get_kernel_info(self.username, id, host, container, port, path, url)
 
-        return container, rule
+    def kill_kernel(self):
+        raise NotImplementedError
