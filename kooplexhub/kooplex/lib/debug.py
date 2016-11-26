e2978938c28c2808f671b2648bb5b83f4e2c5f35
root <root@gmail.com>
2016-11-26 19:08:18 +0100

    Added debug functionality

---
+++ kooplexhub/kooplex/lib/debug.py
@@ -0,0 +1,10 @@
+import datetime as dt
+import inspect
+
+def print_debug(debug,msg):
+    if debug:
+        section = inspect.getouterframes(inspect.currentframe())[1].filename[:-3]
+        subsection = inspect.getouterframes(inspect.currentframe())[1].frame.f_code.co_name
+        now=dt.datetime.now()
+        timestamp="[ %d.%d.%d %d:%d:%d ]"%(now.year,now.day,now.month,now.hour,now.minute,now.second)
+        print("DEBUG - %s: %s - %s\t - %s"%(timestamp,section,subsection,msg))
