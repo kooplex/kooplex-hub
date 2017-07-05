e5adfacf94f1f6b79df49403080b996ebfdfd131
Steger, Jozsef <steger@complex.elte.hu>
2017-07-05 09:12:07 +0200

    new user properties: file_netrc_, file_netrc_exists_

--- kooplexhub/kooplex/hub/models/user.py
+++ kooplexhub/kooplex/hub/models/user.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import User
 from django.db import models
+import os
+
+from kooplex.lib.libbase import get_settings
 
 class HubUser(User):
     #user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -20,3 +23,11 @@ class HubUser(User):
 
     def get_username(self):
         return self.username
+
+    @property
+    def file_netrc_(self):
+        return os.path.join(get_settings('users', 'srv_dir', None, ''), 'home', self.username, '.netrc' )
+
+    @property
+    def file_netrc_exists_(self):
+        return os.path.exists(self.file_netrc_)
