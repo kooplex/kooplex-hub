81404de1a57fea2c9ca3410cbaa2d539b9edaba9
Steger, Jozsef <steger@complex.elte.hu>
2017-10-17 12:19:21 +0200

    show biography in add member form

--- kooplexhub/kooplex/hub/admin.py
+++ kooplexhub/kooplex/hub/admin.py
@@ -78,7 +78,7 @@ reset_password.short_description = 'Reset Password'
 class HubUserAdmin(admin.ModelAdmin):
     list_display = ('username', 'first_name', 'last_name', 'gitlab_id')
     readonly_fields = ('uid', 'gid', 'gitlab_id', 'password')
-    fieldsets = ( (None, { 'fields': ('username', ( 'first_name', 'last_name'), 'email', 'user_permissions', 'uid', 'gid', 'gitlab_id', 'password') }),    )
+    fieldsets = ( (None, { 'fields': ('username', ( 'first_name', 'last_name'), 'bio', 'position', 'location', 'email', 'user_permissions', 'uid', 'gid', 'gitlab_id', 'password') }),    )
     change_list_template = "admin/change_list_hubuser.html"
     actions = [reset_password, ]
 
