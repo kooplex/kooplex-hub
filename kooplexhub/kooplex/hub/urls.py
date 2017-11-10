6fcc7fcd4ede10d05b76d93b250e939067ca2642
jegesm <jegesm@gmail.com>
2017-11-10 08:42:49 +0100

    design in frontpages

--- kooplexhub/kooplex/hub/urls.py
+++ kooplexhub/kooplex/hub/urls.py
@@ -1,6 +1,11 @@
 ﻿from django.conf.urls import url, include
 
 from .views import *
+from django.shortcuts import render
+
+
+def tutorial(request):
+    return render(request, 'app/tutorial.html')
 
 urlpatterns = [
     url(r'^', include('kooplex.hub.views.home')),
@@ -8,4 +13,5 @@ urlpatterns = [
     url(r'^notebooks', include('kooplex.hub.views.notebooks')),
     url(r'^list', include('kooplex.hub.views.upload')),
     url(r'^changepassword', include('kooplex.hub.views.changepassword')),
+    url(r'^tutorial', tutorial, name="tutorial"),
 ]
