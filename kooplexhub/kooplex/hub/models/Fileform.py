15869870988e192775c94d4bb5250011d00c66a9
Steger, Jozsef <steger@complex.elte.hu>
2018-01-09 15:20:06 +0100

    drastic changes of the model (container)

--- kooplexhub/kooplex/hub/models/Fileform.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-
-class Fileform(models.Model):
-    docfile = models.FileField(upload_to=' ')
--- kooplexhub/kooplex/hub/models/container.py
+++ kooplexhub/kooplex/hub/models/container.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 
 from .user import HubUser
 from .project import Project
+from .volume import Volume, VolumeProjectBinding
 
 class Container(models.Model):
     id = models.UUIDField(primary_key = True)  #FIXME: TO BE REPLACED BY IntegerField
@@ -14,13 +15,17 @@ class Container(models.Model):
     image = models.CharField(max_length = 200, null = True)
     ip = models.GenericIPAddressField()
     environment = models.TextField(null = True)
-    binds = models.TextField(null = True)
     command = models.TextField(null = True)
+
+    container_type = models.ForeignKey(ContainerType, null = False)
+
+    proxy_path = models.CharField(max_length = 200)
 
     launched_at = models.DateTimeField(default = timezone.now)
     state = models.CharField(max_length = 15, null = True)
 
 #FIXME: these two are to be removed
+    binds = models.TextField(null = True)
     project_owner = models.CharField(max_length=200, null=True)
     project_name = models.CharField(max_length=200, null=True)
 ###################################################################
@@ -34,6 +39,9 @@ class Container(models.Model):
     def save(self):
         self.image = self.project.image
         models.Model.save(self)
+        for vpb in VolumeProjectBinding.objects.filter(project = self.project):
+            vcb = VolumeContainerBinding(container = self, volume = vpb.volume)
+            vcb.save()
 
 ##    def from_docker_dict(docker, dict):
 ##        c = Container()
@@ -51,4 +59,25 @@ class Container(models.Model):
 ##        c.ports = dict['Ports']          # TODO
 ##        c.state = dict['State']    # created|restarting|running|paused|exited|dead
 ##        return c
+
+class ContainerType(models.Model):
+    id = models.AutoField(primary_key = True)
+    name = models.CharField(max_length = 32)
+
+class VolumeContainerBinding(models.Model):
+    id = models.AutoField(primary_key = True)
+    volume = models.ForeignKey(Volume, null = False)
+    container = models.ForeignKey(Container, null = False)
+
+    def __str__(self):
+       return "%s-%s" % (self.container.name, self.volume.name)
+
+
+def init_model():
+    containertypes = [ 'notebook', 'dashboard' ]
+    for ct in containertype:
+        cti = ContainerType.objects.get(name = ct)
+        if cti is None:
+            cti = ContainerType(name = cti)
+            cti.save()
 
