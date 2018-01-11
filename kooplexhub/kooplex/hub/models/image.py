4056d0766d1838cb4b81ddafd1054f7e5c11e1a0
Steger, Jozsef <steger@complex.elte.hu>
2018-01-11 14:34:45 +0100

    initialise instances using the docker API

--- kooplexhub/kooplex/hub/models/image.py
+++ kooplexhub/kooplex/hub/models/image.py
@@ -1,11 +1,16 @@
 from django.db import models
-
+from kooplex.lib.docker import Docker
 
 class Image(models.Model):
     id = models.AutoField(primary_key = True)
     name = models.CharField(max_length = 32)
 
+    def __str__(self):
+        return self.name
+
 
 def init_model():
-#TODO: a docker API
-    pass
+    Image.objects.all().delete()
+    for image_name in Docker().list_imagenames():
+        Image(name = image_name).save()
+
