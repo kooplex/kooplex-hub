bd36e02c5628b973532e8b456ac325638c312eaf
Steger, Jozsef <steger@complex.elte.hu>
2018-01-11 15:11:46 +0100

    minor changes to conform to model

--- kooplexhub/kooplex/lib/auth.py
+++ kooplexhub/kooplex/lib/auth.py
@@ -1,8 +1,8 @@
-﻿from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 from kooplex.lib.gitlab import Gitlab
 from kooplex.lib.gitlabadmin import GitlabAdmin
 from kooplex.lib.debug import *
-from kooplex.hub.models.user import User as HubUser #FIXME:
+from kooplex.hub.models.user import User
 
 class Auth(object):
     """description of class"""
@@ -13,26 +13,27 @@ class Auth(object):
         res, u = g.authenticate_user(username, password)
         if u is not None:
             try:
-                user = HubUser.objects.get(username=username)
+                return User.objects.get(username = username)
             except User.DoesNotExist:
-                # Create a new user. Note that we can set password
-                # to anything, because it won't be checked; the password
-                # from settings.py will.
-                user = HubUser(username=username, password='get from settings.py', email=u['email'])
-                user.is_staff = True
-                user.is_superuser = u['is_admin']
-                user.gitlab_id = u['id']
+                # Authenticate user does not exist
+                # Create a new user.
+                user = User(
+                    username = username, 
+                    email = u['email'], 
+                    is_superuser = u['is_admin'], 
+                    gitlab_id = u['id'])
                 user.save()
-            return user
-        return None
+                return user
 
     def get_user(self, user_id):
-        print_debug("%d"%user_id)
+        print_debug("%d" % user_id)
         try:
-            return User.objects.get(pk=user_id)
+            # Our user model descends from django.user
+            return User.objects.get(user_ptr = user_id)
         except:
-            return None
+            pass
 
-    def add_user(self, username, password, name, email, projects_limi=100):
+    def add_user(self, username, password, name, email, projects_limi = 100):
+        raise Exception("lib.auth Auth.add_user called")
         gad = GitlabAdmin()
         gad.create_user(username, password, name, email, projects_limi=projects_limi)
