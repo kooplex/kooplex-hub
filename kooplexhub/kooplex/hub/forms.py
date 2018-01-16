219989d7d40ef5b1dada1496c0c71b65d64954cc
David Visontai <jegesm@gmail.com>
2018-01-16 16:26:09 +0100

    login script fixing

--- kooplexhub/kooplex/hub/forms.py
+++ kooplexhub/kooplex/hub/forms.py
@@ -1,16 +1,21 @@
 """
 Definition of forms.
 """
-
+import logging
+logger = logging.getLogger(__name__)
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.utils.translation import ugettext_lazy as _
+from django.shortcuts import redirect
+
+from kooplex.hub.models import User
+from kooplex.lib import Gitlab
 
 class BootstrapAuthenticationForm(AuthenticationForm):
     """Authentication form which uses boostrap CSS."""
     username = forms.CharField(
         max_length = 30, 
-        widget = forms.TextInput( { 'class': 'form-control' } )
+        widget = forms.TextInput( { 'class': 'form-control'} )
     )
     password = forms.CharField(
         max_length = 128, 
@@ -19,13 +24,37 @@ class BootstrapAuthenticationForm(AuthenticationForm):
 
 #HACK
     def __init__(self, req, *k, **kw):
-        if req.method != 'GET':
-            raise Exception(str(req) + str(k) + str(kw))
+        #is_bound = True
+        #_errors = None
+        #empty_permitted = True
+#        super(AuthenticationForm, self)
         AuthenticationForm.__init__(self, req, *k, **kw)
+        logger.debug("Login tortent")
+        if req.method == 'POST':
+            username = req.POST['username']
+            password = req.POST['password']
+            g = Gitlab()
+            res, u = g.authenticate_user(username, password)
+            if u is not None:
+                try:
+                    User.objects.get(username=username)
+                except User.DoesNotExist:
+                    # Authenticate user does not exist
+                    # Create a new user.
+                    user = User(
+                        username=username,
+                        email=u['email'],
+                        is_superuser=u['is_admin'],
+                        gitlab_id=u['id'])
+                    user.save()
+            redirect('projects')
+        else:
+            AuthenticationForm.__init__(self, req, *k, **kw)
 
     def confirm_login_allowed(self, user):
-        raise Exception("almafa")
-        raise forms.ValidationError( _("ALMA" + str(user)), code = 'debug' )
+        #raise Exception("almafa")
+        #raise forms.ValidationError( _("ALMA" + str(user)), code = 'debug' )
+        return True
 #ENDHACK
 
 class BootstrapPasswordChangeForm(PasswordChangeForm):
