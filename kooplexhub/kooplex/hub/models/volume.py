6bb8b35edc3ec9e86d79dcaa8b9167d94cab4513
Steger, Jozsef <steger@complex.elte.hu>
2018-01-20 09:01:08 +0100

    combine mountpoint and former volume models together: new notions: StorageVolume FunctionalVolume

--- kooplexhub/kooplex/hub/models/volume.py
+++ kooplexhub/kooplex/hub/models/volume.py
@@ -3,6 +3,7 @@
 from django.db import models
 
 from .project import Project
+from .user import User
 
 class Volume(models.Model):
     id = models.AutoField(primary_key = True)
@@ -12,6 +13,41 @@ class Volume(models.Model):
 
     def __str__(self):
         return self.displayname
+
+    def mode(self, user):
+        if hasattr(self, 'owner'):
+            if self.owner == user:
+                return "rw"
+        try:
+            upvb = UserPrivilegeVolumeBinding.objects.get(volume = self, user = user)
+            return "rw" if upvb.readwrite else "ro"
+        except UserPrivilegeVolumeBinding.DoesNotExist:
+            return "ro"
+
+class FunctionalVolume(Volume):
+    owner = models.ForeignKey(User, null = False)
+ 
+class StorageVolume(Volume):
+    groupid = models.IntegerField(null = True)
+
+class UserPrivilegeVolumeBinding(models.Model):
+    id = models.AutoField(primary_key = True)
+    volume = models.ForeignKey(Volume, null = True)
+    user = models.ForeignKey(User, null = True)
+    readwrite = models.BooleanField(default = False)
+
+    def __str__(self):
+        return "%s@%s" % (self.user, self.volume)
+
+class VolumeProjectBinding(models.Model):
+    id = models.AutoField(primary_key = True)
+    volume = models.ForeignKey(Volume, null = False)
+    project = models.ForeignKey(Project, null = False)
+    #readwrite = models.BooleanField(default = False)
+
+    def __str__(self):
+       return "%s-%s" % (self.project.name, self.volume.name)
+
 
 #    @property
 #    def container_mountpoint_(self):
@@ -31,17 +67,4 @@ class Volume(models.Model):
 #
 #    def delete(self):
 #        pass
-
-
-class VolumeProjectBinding(models.Model):
-    id = models.AutoField(primary_key = True)
-    volume = models.ForeignKey(Volume, null = False)
-    project = models.ForeignKey(Project, null = False)
-
-    def __str__(self):
-       return "%s-%s" % (self.project.name, self.volume.name)
-
-#    @property
-#    def accessrights_(self):
-#        return 'rw'
 
