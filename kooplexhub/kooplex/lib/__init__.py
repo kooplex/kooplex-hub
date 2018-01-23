bc1b3ad24abae8587bc5177b6eaa44e22c211fe8
Steger, Jozsef <steger@complex.elte.hu>
2018-01-23 23:37:37 +0100

    rename lldap to ldap

--- kooplexhub/kooplex/lib/__init__.py
+++ kooplexhub/kooplex/lib/__init__.py
@@ -3,5 +3,5 @@ from .libbase import get_settings, keeptrying, bash
 from .docker import Docker
 from .gitlab import  Gitlab
 from .gitlabadmin import  GitlabAdmin
-from .lldap import Ldap
+from .ldap import Ldap
 
