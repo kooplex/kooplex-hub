7cd2e287820a0948daa2a24113441ea3e908394a
Steger, Jozsef <steger@complex.elte.hu>
2018-02-01 14:33:38 +0100

    provide the date when sending an e-mail

--- kooplexhub/kooplex/lib/sendemail.py
+++ kooplexhub/kooplex/lib/sendemail.py
@@ -1,17 +1,33 @@
+'''
+@summary: send e-mails to users in the name of the administrator
+@author: Jozsef Steger
+'''
+import logging
 import time
 import smtplib
 from email.mime.text import MIMEText
-import logging
+from email.utils import formatdate
 
 from kooplex.lib import get_settings
 
 logger = logging.getLogger(__name__)
 
 def _send(to, subject, message):
+    '''
+    @summary: helper method to send a single message
+    @param to: the recepient's e-mail address
+    @type to: str
+    @param subject: the subject of the mail
+    @type subject: str
+    @param message: the mail content
+    @type message: str
+    @returns: 0 if succeeded, 1 otherwise
+    '''
     msg = MIMEText(message)
     msg['Subject'] = subject
     msg['From'] = get_settings('hub', 'adminemail')
     msg['To'] = to
+    msg['Date'] = formatdate(localtime = True)
 
     try:
         s = smtplib.SMTP(get_settings('hub', 'smtpserver'))
@@ -24,6 +40,12 @@ def _send(to, subject, message):
         return 1
 
 def send_new_password(user):
+    '''
+    @summary: send a message that a new password is generated.
+    @param user: recipient
+    @type user: kooplex.hub.models.User
+    @returns: 0 if suceeded 0 otherwise
+    '''
     subject = 'Your kooplex account is created'
     message = """
 Dear %s %s,
@@ -42,6 +64,13 @@ Best regards,
     return status
 
 def send_error_report(status, extra = 'na'):
+    '''
+    @summary: send an error report to admin e-mail address
+    @param status: a short description
+    @type status: str
+    @param extra: any additional info, default is "na"
+    @type extra: str
+    '''
     message = """
 Instance: %s
 Failure: %s
@@ -50,6 +79,14 @@ Extra: %s
     _send(to = get_settings('hub', 'adminemail'), subject = '[SMTP error]', message = message)
 
 def send_token(user, token):
+    '''
+    @summary: send a new token message
+    @param user: recipient
+    @type user: kooplex.hub.models.User
+    @param token: the secret
+    @type token: str
+    @returns: 0 if suceeded 0 otherwise
+    '''
     subject = 'Kooplex account password reset request'
     message = """
 Dear %s %s,
