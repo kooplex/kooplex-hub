fdef9478a71e0a731c98858fcbc204a0919be26f
Steger, Jozsef <steger@complex.elte.hu>
2018-05-10 17:54:53 +0200

    still an ugyl ws reporter

--- kooplexhub/kooplex/hub/ws.py
+++ kooplexhub/kooplex/hub/ws.py
@@ -1,12 +1,13 @@
 import logging
 import json
 import time
+import os
 
 from django.conf.urls import url
 from django.http import HttpRequest, HttpResponse
 from django.contrib.auth import authenticate, login, logout
 
-from kooplex.hub.models import User, ScopeType, Project, DashboardReport, HtmlReport, filter_report
+from kooplex.hub.models import User, ScopeType, Project, DashboardReport, HtmlReport, filter_report, get_project
 from kooplex.logic.impersonator import publish_htmlreport
 from kooplex.lib.filesystem import copy_reportfiles_in_place, cleanup_reportfiles
 
@@ -33,10 +34,24 @@ def auth(request):
         return HttpResponse(json.dumps({ 'Error': str(e) }))
     return HttpResponse(json.dumps({ 'Status': 200 }))
 
+
+def _translate(whoami, project, filename):
+    if filename.startswith('git/'):
+        return { 'volname': 'git', 'filename': filename[4:], 'filename_in_hub': '/mnt/volumes/git/%s/%s/%s' % (whoami.username, project.name_with_owner, filename[4:]) }
+    elif filename.startswith('share/'):
+        return { 'volname': 'share', 'filename': filename[6:], 'filename_in_hub': '/mnt/volumes/share/%s/%s' % (project.name_with_owner, filename[6:]) }
+    else:
+        return { 'volname': 'home', 'filename': filename, 'filename_in_hub': '/mnt/volumes/home/%s/%s' % (whoami.username, filename) }
+
 def publish(request):
+#FIXME: filesystem stuff here!!!
+    from distutils import dir_util
+    from distutils import file_util
+
 #NOTE: some part of the code is a duplicate (view/publish.py). May make sense to converge
     assert isinstance(request, HttpRequest) and request.method == 'POST'
     try:
+        whoami = request.user
         projectname = request.POST.get('projectname', '')
         projectownerusername = request.POST.get('projectownerusername', '')
         reportname = request.POST.get('reportname', '')
@@ -45,10 +60,12 @@ def publish(request):
         reportpassword = request.POST.get('reportpassword', '')
         description = request.POST.get('description', 'An automated report issued by %s' % request.user)
         notebook_filename = request.POST.get('notebook_filename', '')
+        #attach_files = request.POST.getlist('other_files')
+        attach_files = request.POST.get('other_files', '').split(',') # FIXME
         deleteformerreports = bool(request.POST.get('deleteformerreports', 'True'))
         scope = ScopeType.objects.get(name = reportscope)
         projectowner = User.objects.get(username = projectownerusername)
-        project = Project.objects.get(name = projectname, owner = projectowner)
+        project = get_project(name = projectname, owner = projectowner)
         if projectowner != request.user and request.user not in project.collaborators:
             logger.error('ws unauthorized to publish: user: %s project: %s (owned by %s)' % (request.user, project, projectowner))
             return HttpResponse(json.dumps({ 'Error': 'You are not authorized to publish that particular report' }))
@@ -69,35 +86,49 @@ def publish(request):
                 logger.info('ws deleted report: %s' % r)
         else:
             logger.debug('asked to keep former reports')
-#FIXME:
-        t = {
-            'filename_in_mount': '/mnt/.volumes/home/steger/%s' % notebook_filename,
-            'filename_in_hub': '/mnt/volumes/home/steger/%s' % notebook_filename,
-            'filename_in_container': '/home/steger/%s' % notebook_filename,
-            'volname': 'home',
-        }
-####################
+        t = _translate(whoami, project, notebook_filename)
+        filename_in_container = '/home/%s/%s' % (whoami.username, notebook_filename),
         report = reportclass(
             creator = request.user,
             name = reportname,
             description = description,
             ts_created = int(time.time()),
             project = project,
-            notebook_filename = t['filename_in_mount'],
+            notebook_dirname = t['volname'],
+            notebook_filename = t['filename'],
             scope = scope,
             password = reportpassword
         )
         report.filename_in_hub = t['filename_in_hub']
-        report.filename_in_container = t['filename_in_container']
+        report.filename_in_container = filename_in_container
         report.volname = t['volname']
+        report.save()    #FIXME: menteni kell, hogy legyen id a base-hez view/publish.py
         # conversion and deployment
         if isinstance(report, HtmlReport):
             publish_htmlreport(report)
-            #files = request.POST.getlist('other_files')
-            #copy_reportfiles_in_place(report, files)
         elif isinstance(report, DashboardReport):
-            files = [] #FIXME: file attachments are not handled yet
-            copy_reportfiles_in_place(report, files)
+            attach_files = [] #FIXME: file attachments are not handled yet
+        logger.debug('copy report files %s to report %s' % (attach_files, report))
+#FIXME: HACK
+        #copy_reportfiles_in_place(report, attach_files)
+        for fn in attach_files:
+            if len(fn) == 0:
+                continue
+            t = _translate(whoami, project, fn)
+            dest = os.path.join(report.report_root, fn)
+            src = t['filename_in_hub']
+            logger.debug('copy attachment report %s [req %s] %s -> %s' % (report, fn, src, dest))
+            if os.path.isdir(src):
+                logger.debug("%s is a folder FIXME" % src)
+                pass
+            else:
+                logger.debug("%s is a file" % src)
+                dir_target = os.path.dirname(dest)
+                logger.debug("dir_target %s" % dir_target)
+                dir_util.mkpath(dir_target)
+                logger.debug("mkdired")
+                file_util.copy_file(src, dir_target)
+                logger.debug("copied")
         report.save()
         logger.info('new report %s' % report)
     except Exception as e:
