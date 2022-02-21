c7c5be9df3cbaed9b98ff66b1559400de6265771
Steger, Jozsef <steger@complex.elte.hu>
2022-02-21 12:53:47 +0100

    unique constraint

--- kooplexhub/education/models/coursecontainerbinding.py
+++ kooplexhub/education/models/coursecontainerbinding.py
@@ -12,6 +12,9 @@ class CourseContainerBinding(models.Model):
     course = models.ForeignKey(Course, null = False, on_delete = models.CASCADE)
     container = models.ForeignKey(Container, null = False, on_delete = models.CASCADE)
 
+    class Meta:
+        unique_together = [['course', 'container']]
+
     def __str__(self):
         return "<CourseContainerBinding %s-%s>" % (self.course, self.container)
 
