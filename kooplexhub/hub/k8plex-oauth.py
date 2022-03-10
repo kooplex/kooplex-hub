d0707756433df5b91b96244c84e6bcc0c26d898d
Steger, Jozsef <steger@complex.elte.hu>
2022-03-10 15:29:32 +0100

    FQDN in favour of URL_PROFILE

--- kooplexhub/hub/k8plex-oauth.py
+++ kooplexhub/hub/k8plex-oauth.py
@@ -4,7 +4,7 @@ import os
 from urllib.parse import urlencode
 from social_core.backends.open_id_connect import BaseOAuth2
 
-from kooplexhub.settings import KOOPLEX_OID_AUTHORIZATION_URL, KOOPLEX_OID_ACCESS_TOKEN_URL, URL_PROFILE
+from kooplexhub.settings import KOOPLEX_OID_AUTHORIZATION_URL, KOOPLEX_OID_ACCESS_TOKEN_URL, FQDN_AUTH
 
 logger = logging.getLogger(__name__)
 
@@ -25,6 +25,6 @@ class KooplexOpenID(BaseOAuth2):
                  }
 
     def user_data( self, access_token, *args, **kwargs):
-        url = URL_PROFILE + '?' + urlencode({'access_token': access_token})
+        url = f'https://{FQDN_AUTH}/oauth/profile?' + urlencode({'access_token': access_token})
         return self.get_json(url)
 
