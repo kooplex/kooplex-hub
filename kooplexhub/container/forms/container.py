585dfc9cd928e4cb174b39d515928262af344912
root <root@hub-0.hub.k8plex-test.svc.cluster.local>
2022-11-04 18:39:02 +0100

    form fix

--- kooplexhub/container/forms/container.py
+++ kooplexhub/container/forms/container.py
@@ -10,17 +10,18 @@ from kooplexhub.lib import my_alphanumeric_validator
 class FormContainer(forms.Form):
     friendly_name = forms.CharField(
             max_length = 200, required = True,
+            label = 'Name', #FIXME: when model refactored, ie friendly_name becomes name, it can be removed
             help_text = _('A short friendly name makes your life easier to find your container environment.'), 
-            widget = forms.TextInput(attrs = {"data-intro": "#friendlyname"})
+            widget = forms.TextInput(attrs = {"data-intro": "#friendlyname"}) #FIXME: is it still used? was not it chardin
             )
-    name = forms.CharField(
-            max_length = 100, required = True,
-            help_text = _('A container name, the system uses to identify your environment. It has to be unique among your container names.'), 
-            validators = [
-                    my_alphanumeric_validator('Enter a valid container name containing only letters and numbers.'),
-                ],
-            widget = forms.TextInput(attrs = {"data-intro": "#name"})
-            )
+#    name = forms.CharField(
+#            max_length = 100, required = True,
+#            help_text = _('A container name, the system uses to identify your environment. It has to be unique among your container names.'), 
+#            validators = [
+#                    my_alphanumeric_validator('Enter a valid container name containing only letters and numbers.'),
+#                ],
+#            widget = forms.TextInput(attrs = {"data-intro": "#name"})
+#            )
     image = forms.ModelChoiceField(
             queryset = Image.objects.filter(imagetype = Image.TP_PROJECT, present = True), 
             help_text = _('Please select to associate an image to your new container environment. During selection a short description of each image is shown to help you decide.'), required = True, 
@@ -32,6 +33,20 @@ class FormContainer(forms.Form):
             label = 'Node', #FIXME: when model refactored, ie friendly_name becomes name, it can be removed
             help_text = _('Choose a node where to launch the environment. '), 
             widget = forms.TextInput(attrs = {"data-intro": "#node"}) #FIXME: is it still used? was not it chardin
+            )
+    
+    cpurequest = forms.IntegerField(
+            required = False,
+            label = 'CPU', #FIXME: when model refactored, ie friendly_name becomes name, it can be removed
+            help_text = _('Choose a cpu requestronment. '), 
+            #widget = forms.TextInput(attrs = {"data-intro": "#node"}) #FIXME: is it still used? was not it chardin
+            )
+
+    memoryrequest = forms.IntegerField(
+            required = False,
+            label = 'Memory', #FIXME: when model refactored, ie friendly_name becomes name, it can be removed
+            help_text = _('Choose a memory where to launch the environment. '), 
+            #widget = forms.TextInput(attrs = {"data-intro": "#node"}) #FIXME: is it still used? was not it chardin
             )
 
     def descriptions(self):
@@ -45,8 +60,8 @@ class FormContainer(forms.Form):
 #            args = ({'friendly_name': container.friendly_name, 'image': container.image, 'name': 'dummy' }, )
             args = ({'friendly_name': container.friendly_name, 'image': container.image, 'node': container.node }, )
         super(FormContainer, self).__init__(*args, **kwargs)
-        if container:
-            self.fields['name'].widget = forms.HiddenInput()
+#        if container:
+#            self.fields['name'].widget = forms.HiddenInput()
         for field in self.fields:
             help_text = self.fields[field].help_text
             self.fields[field].help_text = None
