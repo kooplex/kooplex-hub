d7f0896984cfc5e601ecba4bc7e322888958278a
jegesm <david.visontai@complex.elte.hu>
2025-02-05 10:53:37 +0100

    token and monitoring view comment

--- kooplexhub/hub/models/token.py
+++ kooplexhub/hub/models/token.py
@@ -1,17 +1,30 @@
 import logging
 import unidecode
 
+from django.contrib.auth.models import User
+
 from django.db import models
-#from django.contrib.auth.models import User
-#from hub.models.service import Service
 
 logger = logging.getLogger(__name__)
 
+#TODO accommodate Canvas and other tokens as well
+
+class TokenType(models.Model):  
+    name = models.CharField(max_length = 64, null = False)
+    description = models.CharField(max_length = 512, null = True)
+    #service = models.ForeignKey('Service', on_delete = models.CASCADE, null = False)
+
+    def __str__(self):
+        return self.name
+
 class Token(models.Model):  
-    #user = models.ForeignKey(User, on_delete = models.CASCADE)
+    user = models.ForeignKey(User, on_delete = models.CASCADE, null = False)
     #name = models.CharField(max_length = 64, null = True) # unique?
-    #type = models.ForeignKey(Service, on_delete = models.CASCADE)
-    value = models.CharField(max_length = 512, null = True)
+    type = models.ForeignKey(TokenType, on_delete = models.CASCADE)
+    value = models.CharField(max_length = 1024, null = True)
+
+    def __str__(self):
+        return f"type: {self.type}, value: {self.value}"
 
 
 
