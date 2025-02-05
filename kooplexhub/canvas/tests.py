2f2f00978009bbc5989009a8d6eeaa58a89305e4
jegesm <david.visontai@complex.elte.hu>
2025-02-05 09:18:35 +0100

    added Canvas app

---
+++ kooplexhub/canvas/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+from .models.canvas import Canvas
+
+# Create your tests here.
+class CanvasTestCase(TestCase):
+    def test_canvas(self):
+        pass
+
+    def test_canvas_course(self):
+        pass
