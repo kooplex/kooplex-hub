b86d94ad6290a8142c3c09380d3c9349f8f293fe
Stéger, József <steger.jozsef@gmail.com>
2025-10-20 18:43:20 +0200

    fixes

--- kooplexhub/project/models/userprojectbinding_signals.py
+++ kooplexhub/project/models/userprojectbinding_signals.py
@@ -28,49 +28,31 @@ def assert_single_creator(sender, instance, **kwargs):
 @receiver(pre_save, sender = UserProjectBinding)
 def grantaccess_project(sender, instance, **kwargs):
     from ..tasks import grant_access
-    p = instance.project
     if instance.id is None:
         is_creator = instance.role == UserProjectBinding.Role.CREATOR
         if is_creator:
-            acl = { 'users_rw': [instance.user.id] }
-            creator_username = instance.user.username
+            group_created=False
         else:
-            creator_username = p.creator.username
-            with transaction.atomic():
-                group, group_created = Group.objects.select_for_update().get_or_create(name = instance.groupname, grouptype = Group.TP_PROJECT)
+            group, group_created = Group.objects.get_or_create(name = instance.groupname, grouptype = Group.TP_PROJECT)
             if group_created:
                 creator = UserProjectBinding.objects.get(project = instance.project, role = UserProjectBinding.Role.CREATOR).user
-                UserGroupBinding.objects.get_or_create(user = creator, group = group)
-                acl = { 'groups_rw': [group.id] }
-            else:
-                acl = None
+                UserGroupBinding.objects.get_or_create(user = instance.project.creator, group = group)
             UserGroupBinding.objects.get_or_create(user = instance.user, group = group)
-        if acl:
-            grant_access(folders=[ fs.path_project(p), fs.path_report_prepare(p) ],acl=acl)
+        transaction.on_commit(lambda: grant_access(project=instance.project, group_created=group_created))
+
        
 
 @receiver(pre_delete, sender = UserProjectBinding)
 def revokeaccess_project(sender, instance, **kwargs):
-    from ..tasks import revoke_access
     if instance.role != UserProjectBinding.Role.CREATOR:
-        group = Group.objects.get(name = instance.groupname, grouptype = Group.TP_PROJECT)
-        UserGroupBinding.objects.get(user = instance.user, group = group).delete()
-        creator = instance.project.creator.username if instance.project.creator is not None else "creator_missing"
-        revoke_access(user_id=instance.user.id, folders=[ fs.path_project(instance.project), fs.path_report_prepare(instance.project) ])
+        UserGroupBinding.objects.get(user = instance.user, group_name = instance.groupname).delete()
 
 
 @receiver(pre_delete, sender = UserProjectBinding)
 def assert_not_shared(sender, instance, **kwargs):
     from ..models import ProjectContainerBinding
     for pcb in ProjectContainerBinding.objects.filter(project = instance.project, container__user = instance.user):
-        pcb.container.mark_restart(f"Revoked access to project {instance.project.name}")
+        pcb.container.mark_restart(f"Revoked access from project {instance.project.name}")
         pcb.delete()
 
 
-@receiver(pre_delete, sender = UserProjectBinding)
-def garbagedir_project(sender, instance, **kwargs):
-    from hub.tasks import archive
-    if instance.role != UserProjectBinding.Role.CREATOR:
-        return
-    archive(folder=fs.path_project(instance.project), tarbal=fs.garbage_project(instance.project), remove=True)
-    archive(folder=fs.path_report_prepare(instance.project), tarbal=fs.garbage_report_prepare(instance.project), remove=True)
--- kooplexhub/project/tasks.py
+++ kooplexhub/project/tasks.py
@@ -1,8 +1,10 @@
 import logging
 import time
 
+from django.db.models import Count
 from channels.layers import get_channel_layer
-from django_huey import db_task, task
+from django_huey import db_task, task, periodic_task
+from huey import crontab
 from asgiref.sync import async_to_sync
 
 from django.contrib.auth.models import User
@@ -13,37 +15,66 @@ from hub.lib import archivedir, extracttarbal
 from hub.lib import grantaccess_user, revokeaccess_user
 from hub.lib import grantaccess_group, revokeaccess_group
 from hub.lib import mkdir, archivedir, rmdir
+from hub.lib.ldap import Ldap
+from kooplexhub.lib import bash
+from .models import Project
+from project import fs
 
 logger = logging.getLogger(__name__)
 
-@task(queue = 'project')
-def grant_access(folders, acl):
-    channel_layer=get_channel_layer()
-    #FIXME: lehetne ACL: [{'folder': '...', 'groups_rw': [...], 'groups_ro': [...]}, ...]
-    for f in folders:
-        for gid in acl.get('groups_rw', []):
-            with transaction.atomic():
-                go = Group.objects.select_for_update().get(id = gid)
-            grantaccess_group(go, f, readonly = False, recursive = True)
-        for uid in acl.get('users_rw', []):
-            grantaccess_user(User.objects.get(id = uid), f, readonly = False, recursive = True)
-
-    async_to_sync(channel_layer.group_send)("project", {
-            "type": "feedback",
-            "feedback": f"Folders {folders} got acl {acl}",
-        })
+@periodic_task(crontab(minute="*/15"), queue='project')
+def sync_project_groups():
+    # ldap project groups
+    l=Ldap()
+    g_ldap = list(filter(lambda x: x.startswith('p-'), l.groups()))
+    # make projects that have collaborators have groups related
+    qs = (
+        Project.objects
+        .annotate(num_bindings=Count('userbindings'))
+        .filter(num_bindings__gt=1)
+    )
+    for p in qs:
+        logger.debug(f'Test groups for project {p}')
+        gn=p.groupname
+        if g:=p.group:
+            if gn in g_ldap:
+                g_ldap.remove(gn)
+                logger.debug(f'Project {p} has group and ldap group: {gn}')
+            else:
+                logger.error(f'Project {p} has group, but ldap entry is missing')
+                l.addgroup(g)
+                #FIXME add userbindings also
+        else:
+            logger.error(f'Project {p} is missing group')
+            Group.objects.create(name=gn, grouptype=Group.TP_PROJECT)
+            if gn in g_ldap:
+                logger.warning(f'ldap entry {gn} was present anyway')
+                g_ldap.remove(gn)
+    # remove unused ldap entries
+    for lg in g_ldap:
+        logger.info(f'Removing unused ldap entry {lg}')
+        try:
+            l.removegroup(Group(name=lg))
+        except Exception as e:
+            logger.critical(e)
     return "Completed"
 
+
 @task(queue = 'project')
-def revoke_access(user_id, folders):
-    u = User.objects.get(id = user_id)
-    for f in folders:
-        revokeaccess_user(u, f)
-    async_to_sync(channel_layer.group_send)("project", {
-            "type": "feedback",
-            "feedback": f"{u}'s acls are removed from folders {folders}",
-        })
+def grant_access(project, group_created):
+    for folder in [ fs.path_project(project), fs.path_report_prepare(project) ]:
+        logger.debug(f"project folder {folder} group? {group_created} (creator: {project.creator}, group: {project.group})")
+        if group_created:
+            grantaccess_group(project.group, folder, readonly = False, recursive = True)
+        else:
+            grantaccess_user(project.creator, folder, readonly = False, recursive = True)
+#    channel_layer=get_channel_layer()
+#    async_to_sync(channel_layer.group_send)("project", {
+#            "type": "feedback",
+#            "feedback": f"Folders {folders} got acl {acl}",
+#        })
     return "Completed"
+
 
 
 
